67313e7e52f3a0e8e1acb68a1ec489cf3c597db0
matt-colen <mzcolen@gmail.com>
2023-11-10 21:43:56 -0600

    Adds post rendering w/ JS & like feature
    Adds post rendering through JS as well as an ability to "like" the posts.

--- index.js
+++ index.js
@@ -29,4 +29,122 @@ const posts = [
   },
 ];
 
-const createPost = (postArray) => {};
+const createPost = (postArray) => {
+  let posts = postArray.map((postObj, index) => {
+    const postName = postObj["name"];
+    const postUsername = postObj["username"];
+    const postLocation = postObj["location"];
+    const postImg = postObj["post"];
+    const postAvatar = postObj["avatar"];
+    const postComment = postObj["comment"];
+    const postLikes = postObj["likes"];
+
+    const postEl = `
+    <!-- Post article  -->
+    <article id="${postName}" class="post">
+      <!-- Post info section of article -->
+      <section class="post-info__container flex flex-centered">
+        <img
+          class="post-info__img avatar-img"
+          src="${postAvatar}"
+          alt="${postName} avatar"
+        />
+        <div class="post-info__author">
+          <h2 class="post-info__name bold">${postName}</h2>
+          <p class="post-info__location">${postLocation}</p>
+        </div>
+      </section>
+
+      <!-- Post img section of article -->
+      <section class="post-img__container">
+        <img
+          class="post-img"
+          src="${postImg}"
+          alt="Painted selfie of ${postName}"
+        />
+      </section>
+
+      <!-- Post message section of article -->
+      <section class="post-message__container">
+        <ul class="post-message__icons-list flex">
+          <li>
+            <button class="post-icon-btn like-btn">
+              <img
+                class="post-icon"
+                src="images/like.svg"
+                alt="Heart icon"
+              />
+            </button>
+          </li>
+          <li>
+            <button class="post-icon-btn">
+              <img
+                class="post-icon"
+                src="images/comment.svg"
+                alt="Comment icon"
+              />
+            </button>
+          </li>
+          <li>
+            <button class="post-icon-btn">
+              <img
+                class="post-icon"
+                src="images/share.svg"
+                alt="Direct message icon"
+              />
+            </button>
+          </li>
+        </ul>
+        <div class="post-message__info">
+          <p><span class="post-likes bold" id="post-likes">${postLikes}</span> likes</p>
+          <p class="post-message">
+            <span class="bold">${postUsername}</span> ${postComment}
+            lol
+          </p>
+        </div>
+      </section>
+    </article>
+    `;
+
+    return postEl;
+  });
+
+  posts = posts.join("");
+
+  renderPosts(posts);
+};
+
+const renderPosts = (posts) => {
+  const postsContainer = document.querySelector("#posts__container");
+
+  postsContainer.innerHTML += posts;
+};
+
+createPost(posts);
+
+const likeBtns = document.querySelectorAll(".like-btn");
+
+const capturePostLikeCount = (e) => {
+  const parentEl = e.target.closest(".post");
+  const likeCountEl = parentEl.querySelector(`#post-likes`);
+
+  incrementLikes(likeCountEl);
+};
+
+const incrementLikes = (likeEl) => {
+  let likeCount = +likeEl.textContent;
+
+  likeCount++;
+
+  renderLikes(likeEl, likeCount);
+};
+
+const renderLikes = (el, count) => {
+  el.textContent = count;
+};
+
+likeBtns.forEach((btn) => {
+  btn.addEventListener("dblclick", (e) => {
+    capturePostLikeCount(e);
+  });
+});
